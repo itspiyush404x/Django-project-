b1bd55d83db6205ade3a6cadd4d8a0d0e116d4cc
Piyush Saini <piyush9680saini@gmail.com>
2026-08-24 15:24:23 +0530

    form data save to json file

--- Backend/main/views.py
+++ Backend/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from hello_world.data import forms
 from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 
 # Create your views here.
@@ -17,6 +18,30 @@ def hello_piyush(request):
 
 def hello_jatin(request):
     return HttpResponse("Hello Jatin")
+
+@csrf_exempt
+def form(request):
+    if request.method == "POST":
+        try:
+            with open(r"C:\Users\C C\OneDrive\Desktop\coaching projects\projects\first_django_project\Backend\main\data.json", "r") as f:
+                file_data = json.load(f)
+        except json.JSONDecodeError:
+            file_data = {}
+        
+        index = max([int(i) for i in file_data]) + 1
+        file_data.update({str(index):json.loads(request.body)})
+
+        with open(r"C:\Users\C C\OneDrive\Desktop\coaching projects\projects\first_django_project\Backend\main\data.json", "w") as f:
+            json.dump(file_data, f, indent=4)
+    
+    return HttpResponse("Form submitted successfully!")
+
+        
+
+        
+            
+
+
 
 
     
