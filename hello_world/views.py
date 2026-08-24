270cfbb88d78a1bc2dfa8f03065f9fbf3ecf6df8
Piyush Saini <piyush9680saini@gmail.com>
2026-08-24 11:39:59 +0530

    new

---
+++ hello_world/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from hello_world.data import forms
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Create your views here.
+@csrf_exempt
+def hello(request):
+    if request.method == "POST":
+        name = request.POST.get("name","Unknown")
+        return HttpResponse(f"Hello {name}!")
+    return HttpResponse("sent post request")
+
+def hello_piyush(request):
+    return HttpResponse("Hello Piyush")
+
+def hello_jatin(request):
+    return HttpResponse("Hello Jatin")
+
+
+    
